7caba3766fc983802d533d4941047b9f09b442e0
tonghui.wu <tonghui.wu@striking.ly>
2022-03-12 22:35:01 +0800

    feat: a minimal kernel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // 不链接 Rust 标准库
+#![no_main] // 禁用所有 Rust 层级的入口点
+
+use core::panic::PanicInfo;
+
+/// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello tonghui! welcome pure os";
+
+#[no_mangle] // 函数名不混淆
+             // 因为编译器会寻找一个名为 `_start`的函数，所以这个函数就是入口点
+extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop {}
 }
