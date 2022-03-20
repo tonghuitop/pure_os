2dbd40cf6eeb1bd3d8651a08f15f99f2ee3f3096
tonghui.wu <tonghui.wu@striking.ly>
2022-03-20 11:58:36 +0800

    feat: add print macro

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,18 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 /// 这个函数将在 panic 时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello tonghui! welcome pure os";
-
-#[no_mangle] // 函数名不混淆
-             // 因为编译器会寻找一个名为 `_start`的函数，所以这个函数就是入口点
+#[no_mangle]
 extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World \n \n ssss");
+    panic!("Some panic message");
     loop {}
 }
